3fb27830b64c2c371ad3391698e6e7fd504bb374
alanacpereira <alana.c.pereira@aluno.senai.br>
2026-04-22 09:46:46 -0300

    react
    iniciando nosso site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+// onde importo meu arquivo css
+import Logo from './componentes/Logo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <Logo></Logo>
       </header>
     </div>
   );
 }
 
 export default App;
+// ARQUIVO PRINCIPAL DO PROJETO
